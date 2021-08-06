8e4cd8e5594c0e1cf69c9a80395093e6ae0d7f4f
Lucy Kendall <lucykendall88@gmail.com>
2021-08-06 23:17:26 +1000

    working bar gamedraw

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-
+;debugger;
 const gameResults = document.querySelector('h4') // provide the status of the game & communicates with player
-const scores = document.getElementsByClassName("score")
+// const scores = document.getElementsByClassName("score")
 const playButton = document.getElementById('Play');
 const resetButton = document.getElementById('Reset')
+const scores = document.getElementsByClassName("score")
 
 let gameActive = true;
 // //require gameActive to determine if a game is currently underway. 
 let gameStatus = ["", "", "", "", "", "", "", "", ""]; 
 // each cell is treated as a string in an array. Currently each cell is empty as no move has been played.
+let matchWon = false
+const gameDraw = !gameStatus.includes(""); //i.e. no empty places left to go
 
 let player1Turn = true
 let player1Score = 0
@@ -17,6 +20,7 @@ let player2Score = 0
 let player1Selection = '' 
 let player2Selection = ''
 let currentPlayer = ''
+let turn = 1
 
 const selectCharacter = () => {
         let selection = '';
@@ -36,12 +40,10 @@ const selectCharacter = () => {
        
 function characterChosenCheck () {
     if (player1Selection === '' || player2Selection === '') {
-        gameResults.innerHTML= "You need to chose a character to play";
-        document.querySelectorAll('.cell').forEach(cell => cell.removeEventListener('click', cellSelectedOutcome))
-        return;
+        gameResults.innerHTML = "You need to choose a character to play"
+        return false;
     } else {
-        document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellSelectedOutcome))
-        return;
+        return true;
     }
 }
 
@@ -92,40 +94,45 @@ chooseButton.addEventListener('click', (event) => {
 // //GAMEPLAY:
 
 playButton.addEventListener('click', (event) => {
-    gameActive = true;
+    if (characterChosenCheck() && gameActive) {
     gameResults.innerHTML= "Player 1 goes first";
+    }
 })
 
 //when player selects a cell, the current player's character string is entered, i.e. if player1's turn and they are 'witch', 
 //if they select cell 2, cell 2 becomes the current player, elimination the possibility of the cell being player again
 
 function cellSelectedOutcome(event) {
-    characterChosenCheck ()
-    gameActive; 
-    const clickedCell = event.target;
-    const cellIndex = clickedCell.getAttribute('data-cell-index');
-
-    gameResults.innerHTML= `It's ${currentPlayer}'s turn`;
-
-    player1Turn = !player1Turn;
-    currentPlayer = player1Turn ? player1Selection : player2Selection; //ternary exp.
-    console.log(!player1Turn)
-    console.log(currentPlayer)
-
-    if (gameStatus[cellIndex] !== "" && !gameActive) {
-        return;
+    checkifDraw() 
+    if (characterChosenCheck() && gameActive) {
+        const clickedCell = event.target;
+        const cellIndex = clickedCell.getAttribute('data-cell-index');
+        
+        if (gameStatus[cellIndex] !== "" && !gameActive) {
+            return;
+        }
+        cellSelected(clickedCell, cellIndex);
     }
-    cellSelected(clickedCell, cellIndex);
-    checkifWin();
+    checkifDraw() 
 }
 
 function cellSelected(clickedCell, cellIndex) {
-    gameActive;
-    gameStatus[cellIndex] = currentPlayer; //current player is pushed into the cell index
-    clickedCell.innerHTML = currentPlayer; //report to player
-    console.log(currentPlayer)
+    checkifDraw() 
+    if (matchWon === false) {
+        gameStatus[cellIndex] = currentPlayer; //current player is pushed into the cell index
+        clickedCell.innerHTML = currentPlayer; //report to player
+        console.log(gameStatus)
+    }
+    checkifDraw()
+    checkifWin()
+    if (matchWon === false) {
+        player1Turn = !player1Turn;
+        currentPlayer = player1Turn ? player1Selection : player2Selection; //ternary exp.
+        gameResults.innerHTML= `It's ${currentPlayer}'s turn`;
+        console.log(gameStatus)
+    }
+    checkifDraw() 
 }
-
 
 const winningConditions = [
     [0, 1, 2], //top left to right
@@ -138,64 +145,54 @@ const winningConditions = [
     [2, 4, 6] //diagonal right to bottom
 ]
 
-function storeScore () {    //Push scores into html
+function updateScore () {    //Push scores into html
     for (let score of scores) {
         let playerCharacter = score.dataset.character //if witch,werewolf or vampire loop
 
-        if (playerCharacter = player1Selection) {
-            totalScore = score.innerText
-        } else if (playerCharacter = player2Selection) {
-            totalScore= score.innerText
-        } else {
-            return;
+        if (playerCharacter === currentPlayer) {
+            let currentScore = parseInt(score.innerText)
+            score.innerText = currentScore + 1
         }
         console.log(playerCharacter)
         console.log(player1Selection)
     }
 }
 
+function checkifDraw() {
+    if (gameDraw) {
+        gameResults.innerHTML = "It's a draw. Hit rematch to play againt or restart game to choose new characters"
+        gameActive = false;
+        return;
+    }
+}
+
 function checkifWin() {
-
-    let matchWon = false
-
     for (let condition of winningConditions) {
 
         if (gameStatus[condition[0]] === gameStatus[condition[1]] && gameStatus[condition[0]] === gameStatus[condition[2]] && gameStatus[condition[0]] !== "") {
             matchWon = true
-            console.log('winner')
             gameResults.innerHTML = `Player ${currentPlayer} has won. Hit rematch to start again or reset to play with new characters!`
 
-            if (currentPlayer = player1Selection) {
+            if (currentPlayer === player1Selection) {
                 player1Score = player1Score += 1
-                storeScore ()
-            } else if (currentPlayer = player2Selection) {
+            } else if (currentPlayer === player2Selection) {
                 player2Score = player2Score += 1
-                storeScore ()
             }
-
-            console.log(`player one's score is ${player1Score}`)
-            console.log(`player two's score is ${player2Score}`)
+            updateScore()
 
             gameActive = false;
             gameStatus = ["", "", "", "", "", "", "", "", ""];
             document.querySelectorAll('.cell').forEach(cell => cell.removeEventListener('click', cellSelectedOutcome));
             return;
             }
-    }
-
-    let gameDraw = !gameStatus.includes(""); //i.e. no empty places left to go
-
-    if (gameDraw) {
-        gameResults.innerHTML = gameResults.innerHTML = `It's a draw. Hit rematch to play againt or restart game to choose new characters`
-        gameActive = false;
-        return;
-    }
+        }
     return currentPlayer
 }
 
 function rematchGame() {
     gameActive = true;
-    currentPlayer = player1Selection
+    matchWon = false;
+    currentPlayer = player1Selection;
     gameStatus = ["", "", "", "", "", "", "", "", ""];
     gameResults.innerHTML = `It's ${currentPlayer}'s turn`;
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
@@ -203,11 +200,23 @@ function rematchGame() {
 }
 
 function resetGame() {
+    console.log('click')
+    matchWon = false;
     gameActive = true;
     gameStatus = ["", "", "", "", "", "", "", "", ""];
     player1Selection = ''
     player2Selection = ''
-    gameResults.innerHTML = gameResults.textContent
+    player1Score = 0
+    player2Score = 0
+
+    for (let score of scores) {
+        score.innerText = 0
+    }
+
+    document.getElementById('Werewolf').disabled = false;
+    document.getElementById('Witch').disabled = false;
+    document.getElementById('Vampire').disabled = false;
+    gameResults.innerHTML = ("Select a player to start")
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
     document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', cellSelectedOutcome));
 }
